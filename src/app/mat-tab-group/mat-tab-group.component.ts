e5ab493da825aceedac9ad5a8ad4aa47cd959294
Wallace Servato Bertolaccini <wallacebertolaccini@gmail.com>
2023-03-02 13:58:19 -0300

    arrumando indice

--- src/app/mat-tab-group/mat-tab-group.component.ts
+++ src/app/mat-tab-group/mat-tab-group.component.ts
@@ -1,20 +1,36 @@
 import { LoadPokemonService } from './../services/load-pokemon.service';
 import { Router } from '@angular/router';
-import { Component, EventEmitter, Output,Input } from '@angular/core';
+import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mat-tab-group',
   templateUrl: './mat-tab-group.component.html',
   styleUrls: ['./mat-tab-group.component.scss']
 })
-export class MatTabGroupComponent {
-
+export class MatTabGroupComponent implements OnInit {
+  lotsOfTabs:any;
   @Input() pokeIndex:number | undefined;
   constructor(private loadPokemonService:LoadPokemonService){
+  }
+
+
+  ngOnInit() {
+    if(this.pokeIndex){
+      this.inicializarTabs(this.pokeIndex);
+    }
 
   }
-  lotsOfTabs = new Array(100).fill(0).map((_, index) => `${index+1}`);
 
+
+  inicializarTabs(index: number | undefined): void {
+    if (index) {
+      const start = Math.max(index - 100, 1);
+      const end = Math.min(index + 100, 898);
+      this.lotsOfTabs = new Array(end - start + 1).fill(start).map((_, i) => `${start + i}`);
+      this.pokeIndex = this.lotsOfTabs.length - 101;
+    }
+
+  }
 
     goToPoke(pokemon:any){
       if (pokemon.srcElement.outerText>0){
